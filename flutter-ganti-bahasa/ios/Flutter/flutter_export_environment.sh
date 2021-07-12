624345d030b67e7546f3cd1a8a7d8d124efae016
Revno Indra Kusuma <53182130+revnoindrakusuma@users.noreply.github.com>
2021-07-12 12:57:41 +0700

    Add files via upload

---
+++ flutter-ganti-bahasa/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\Refno\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\ESA UNGGUL\semester 6\CIM430 Pemrograman Mobile EU101 7690\flutter-ganti-bahasa-master"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
